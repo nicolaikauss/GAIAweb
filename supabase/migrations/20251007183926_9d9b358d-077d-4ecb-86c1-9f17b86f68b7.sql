dc7bdbee9ff15fe49ad2699785550aa7a5ca6ae8
NICOLAI KAUSS <nicolai.kauss@edu.escp.eu>
2026-04-29 12:05:28 +0200

    Delete supabase directory

--- supabase/migrations/20251007183926_9d9b358d-077d-4ecb-86c1-9f17b86f68b7.sql
+++
@@ -1,2 +0,0 @@
--- Enable realtime for artworks table
-ALTER PUBLICATION supabase_realtime ADD TABLE public.artworks;
--- supabase/migrations/20251007223738_479581b9-3a3a-40b9-9cd3-88c62e066790.sql
+++
@@ -1,5 +0,0 @@
--- Add support for multiple images per artwork
-ALTER TABLE public.artworks 
-ADD COLUMN IF NOT EXISTS images TEXT[] DEFAULT '{}';
-
-COMMENT ON COLUMN public.artworks.images IS 'Array of additional image URLs for the artwork';
